b3b9ef140f4a97ec9376964fc48b80ac100521e4
bentzou <bentzou@gmail.com>
2015-10-27 14:56:21 -0700

    sample file toolset.py

---
+++ toolset.py
@@ -0,0 +1,20 @@
+class Toolset:
+   @classmethod
+   def tool_method_no_args(cls):
+      pass
+
+   @classmethod
+   def tool_method_positional_args(cls, pos_arg1, pos_arg2):
+      pass
+
+   @classmethod
+   def tool_method_keyword_args(cls, kw_arg1="default1", kw_arg2="default2"):
+      pass
+
+   @classmethod
+   def tool_method_keyword_args(cls, pos_arg1, pos_arg2, kw_arg1="default1", kw_arg2="default2"):
+      pass
+
+import cli_interface
+if __name__ == '__main__':
+   cli_interface.CliInterface(Toolset).run()
