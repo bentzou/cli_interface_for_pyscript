2354befc1098b7277d66d51f20be05b38e2cd4d5
bentzou <bentzou@gmail.com>
2015-11-12 22:38:19 -0800

    add support for docstrings and help method

--- cli_interface.py
+++ cli_interface.py
@@ -1,19 +1,25 @@
-import inspect
-import json
+from inspect import getargspec
+from inspect import getdoc
+from json import dumps
 import sys
 
 class CliInterface(object):
    def __init__(self, tool_class):
       self.tool_class = tool_class
+      self.commands = sorted(self.tool_class.__dict__.keys() + ["help"])
 
    def usage(self):
       print "Usage: {0} command [args...]".format(sys.argv[0])
       print
       print "Available commands and the parameters:"
 
-      for command in sorted(self.tool_class.__dict__.keys()):
-         if not command.startswith("_"):
-            argspec = inspect.getargspec(getattr(self.tool_class, command))
+      for command in self.commands:
+         if command.startswith("_"):
+            pass
+         elif command == "help":
+            print "   {0:40s} {1}".format("help", "[command]")
+         else:
+            argspec = getargspec(getattr(self.tool_class, command))
 
             if argspec.defaults:
                req_args = argspec.args[1:-len(argspec.defaults)]
@@ -22,11 +28,40 @@ class CliInterface(object):
                req_args = argspec.args[1:]
                opt_args = []
 
-            req_args_str = " ".join(req_args)
-            opt_args_str = " ".join(["[" + opt_arg + "]" for opt_arg in opt_args])
+            req_args_str = " " + " ".join(req_args) if req_args else ""
+            opt_args_str = " " + " ".join(["[" + opt_arg + "]" for opt_arg in opt_args])
 
-            print "   {0:40s} {1} {2}".format(command, req_args_str, opt_args_str)
-      print
+            print "   {0:40s}{1}{2}".format(command, req_args_str, opt_args_str)
+
+   def print_command_help(self, command=None):
+      # check correct help usage
+      if command is list and len(command) > 1:
+         raise Exception("Incorrect # of arguments for help\n".format(command))
+
+      # print usage
+      if not command:
+         self.usage()
+      elif command in self.tool_class.__dict__:
+         print getdoc(getattr(self.tool_class, command))
+      else:
+         raise Exception("{0} is not a valid command\n".format(command))
+
+   def run_command(self, command, arguments):
+      # get argspec
+      argspec = getargspec(getattr(self.tool_class, command))
+
+      if argspec.defaults:
+         req_args = argspec.args[1:-len(argspec.defaults)]
+         opt_args = argspec.args[-len(argspec.defaults):]
+      else:
+         req_args = argspec.args[1:]
+         opt_args = []
+
+      # check if correct # of arguments
+      if len(arguments) < len(req_args) or len(arguments) > len(req_args) + len(opt_args):
+         raise Exception("Incorrect # of arguments for {0}\n".format(command))
+
+      return getattr(self.tool_class, command)(*arguments)
 
    def run(self):
       # get arguments
@@ -36,31 +71,21 @@ class CliInterface(object):
       else:
          command = sys.argv[1]
          arguments = sys.argv[2:]
-
-      try:
-         # check if command exists
-         if command not in self.tool_class.__dict__:
-            raise Exception("{0} is not a valid command\n\n".format(command))
    
-         # check if correct # of arguments
-         argspec = inspect.getargspec(getattr(self.tool_class, command))
-         if argspec.defaults:
-            req_args = argspec.args[1:-len(argspec.defaults)]
-            opt_args = argspec.args[-len(argspec.defaults):]
-         else:
-            req_args = argspec.args[1:]
-            opt_args = []
-
-         if len(arguments) < len(req_args) or len(arguments) > len(req_args) + len(opt_args):
-            raise Exception("Incorrect # of arguments for {0}\n\n".format(command))
-      except Exception, e:
-         sys.stderr.write(str(e))
+      # check if command exists
+      if command != "help" and command not in self.tool_class.__dict__:
+         sys.stderr.write("{0} is not a valid command\n\n".format(command))
          self.usage()
          sys.exit(1)
 
-      # looks good -- run the command
+      # run the command
       try:
-         return_value = getattr(self.tool_class, command)(*arguments)
-         print json.dumps(return_value)
+         if command == "help":
+            self.print_command_help(arguments[0] if arguments else None)
+         else:
+            return_value = self.run_command(command, arguments)
+            if return_value:
+               print dumps(return_value)
       except Exception, e:
-         sys.exit(str(e))
+         sys.stderr.write(str(e))
+         sys.exit(1)
--- toolset.py
+++ toolset.py
@@ -1,19 +1,31 @@
 class Toolset:
    @classmethod
    def tool_method_no_args(cls):
-      pass
+      """
+      This method tool_method_no_args has no args.
+      """
+      print "tool_method_no_args"
 
    @classmethod
    def tool_method_positional_args(cls, pos_arg1, pos_arg2):
-      pass
+      """
+      This method has positional args (pos_arg1, pos_arg2).
+      """
+      print "tool_method_positional_args"
 
    @classmethod
    def tool_method_keyword_args(cls, kw_arg1="default1", kw_arg2="default2"):
-      pass
+      """
+      This method has keyword args (kw_arg1, kw_arg2).
+      """
+      print "tool_method_keyword_args"
 
    @classmethod
-   def tool_method_keyword_args(cls, pos_arg1, pos_arg2, kw_arg1="default1", kw_arg2="default2"):
-      pass
+   def tool_method_positional_and_keyword_args(cls, pos_arg1, pos_arg2, kw_arg1="default1", kw_arg2="default2"):
+      """
+      This method has positional args (pos_arg1, pos_arg2) and keyword args (kw_arg1, kw_arg2).
+      """
+      print "tool_method_positional_and_keyword_args"
 
 if __name__ == '__main__':
    import cli_interface
